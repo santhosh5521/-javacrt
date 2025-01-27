8f53433f8ba930da840a6999391ff2caf063c39e
santhosh5521 <santhoshnethikunta42@gmail.com>
2025-01-27 10:34:32 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,39 @@
+package day6;
+public class Program1 {
+	static void arRR(int[] x) {
+		int len = x.length;// 5
+		int arrLen = len -1;//4
+		int t =x[arrLen];
+		// num[4] = num[3]	
+		// num[3] = num[2]		
+		// num[2] = num[1]		
+		// num[1] = num[0]	
+		for (int i =arrLen; i>=1;i--) {
+			x[i] = x[i - 1];
+		}
+		x[0] = t;
+		System.out.println("\n after 1 right Rotation \n");
+		for(int i = 0; i <= arrLen; i++) {
+			System.out.print(x[i] + " ");
+		}
+	}
+	
+	public static void main (String[] args) {
+		int[] num = {10, 20, 30, 40, 50};
+		System.out.println("Before right Rotation");
+		for (int i =0; i < num.length;i++) {
+			System.out.print(num[i] + " ");
+		}
+		arRR(num);
+		arRR(num);
+		arRR(num);
+		
+		}
+		
+
+	
+	}
+		
+
+          
+	
